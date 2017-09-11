50059b537cb958d3268b9c447eafed31d38bc58b
sunsnow-tail <sunsnow.tail@yahoo.com.au>
2017-09-11 13:19:22 +1000

    create first React component, fixed ignore file

--- CountryWeather.React/src/Country.js
+++ CountryWeather.React/src/Country.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const apiUrl = '';
+const apiUrl = 'http://localhost:3629/api/Countries';
 
 class Country extends Component {
+    constructor(){
+        super();
+        this.state = {
+            data: []
+        };
+    } 
 
-    getCountries = () => {
-        
-    }
+    componentDidMount() {    
+        var that = this;
+      
+        fetch(apiUrl, {
+            headers : { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+                }      
+            })
+        .then(function(response) {
+          if (response.status >= 400) {
+            throw new Error("Bad response from server");
+          }
+          return response.json();
+        })
+        .then(function(data) {
+          that.setState({ data: data });
+        });
+      }
 
     render(){
 
-        this.getCountries();
-
         return(
             <div>
-                <select id="selCountries"/>
+                <select id="selCountries">
+                {this.state.data.map(id =>
+                    <option key={id} value={id}>{id}</option>
+                )}
+                </select>
             </div>
         );
     }
